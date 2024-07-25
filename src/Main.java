36b8098531b3e1cb7e9c6639615f180da21fe185
vidyanand <vidyanandme@hotmail.com>
2024-07-25 17:30:00 +0530

    tree insertion

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.insert(7);
+        tree.insert(5);
+        tree.insert(4);
+        tree.insert(6);
+        tree.insert(2);
+        tree.insert(1);
+        tree.insert(3);
+        tree.insert(9);
+        System.out.println("done");
+
+        }
+}
